613955950568432f7a9de7b0b5a9d818fcb5028e
Cyrus Kurtz <cyrus.bosworth@gmail.com>
2020-02-15 14:13:51 -0500

    saved game to local storage

--- scripts/app.js
+++ scripts/app.js
@@ -1,82 +1,109 @@
-import Deck from "./Deck.js";
-import play from "./play.js";
-import drawDot, {clearBoard} from "./dotMaker.js";
-import flipCard, {flipCardsNoAni, clear} from "./flip.js";
-import {increaseStats, resetStats} from "./stats.js";
-import {setSummary, hideSummary, displaySummary} from "./summary.js";
-import {clearBets, calcWinnings, returnBets} from "./bet.js";
-import {closeModal, openModal} from "./modals.js";
+import Deck from './Deck.js';
+import play from './play.js';
+import drawDot, { clearBoard, drawDotNoAni } from './dotMaker.js';
+import flipCard, { flipCardsNoAni, clear } from './flip.js';
+import { increaseStats, resetStats } from './stats.js';
+import { setSummary, hideSummary, displaySummary } from './summary.js';
+import { clearBets, calcWinnings, returnBets } from './bet.js';
+import { closeModal, openModal } from './modals.js';
 
-const playBtn = document.querySelector("#play-button");
-const skipBtn = document.querySelector("#skip-button");
-const newBtn = document.querySelector("#new-shoe-button");
-const helpBtn = document.querySelector("#help-button");
+const playBtn = document.querySelector('#play-button');
+const skipBtn = document.querySelector('#skip-button');
+const newBtn = document.querySelector('#new-shoe-button');
+const helpBtn = document.querySelector('#help-button');
+const stats = document.querySelector('#stats');
 
-console.log('hello');
-let deck = new Deck();
 let games = [];
-
-function init() {
-  deck = new Deck();
-  deck.cut(Math.floor(Math.random() * 364) + 51);
-  deck.burn();
-  while (deck.hasCards) {
-    games.push(play(deck));
-  }
-}
+let gameCount = 0;
+let playing = false;
 
 init();
-let gameCount = 0;
 
-let playing = false;
-playBtn.addEventListener("click", async () => {
-  if (playing) return;
-  playBtn.classList.add("disabled");
-  skipBtn.classList.add("disabled");
-  hideSummary();
-  playing = true;
-  await flipCard(games[gameCount]);
-  playing = false;
-  playBtn.classList.remove("disabled");
-  skipBtn.classList.remove("disabled");
-  drawDot(games[gameCount]);
-  increaseStats(games[gameCount].winner);
-  setSummary(games[gameCount]);
-  displaySummary();
-  calcWinnings(games[gameCount]);
-  clearBets();
+function init() {
+	playing = false;
+	if (localStorage.getItem('gameCount')) {
+		clearBoard();
+		games = JSON.parse(localStorage.getItem('games'));
+		gameCount = Number(localStorage.getItem('gameCount'));
 
-  gameCount++;
-  if (gameCount === games.length - 1) {
-    openModal("shuffle");
-  }
+		for (let i = 0; i < gameCount; i++) {
+			drawDotNoAni(games[i]);
+			increaseStats(games[i].winner);
+			console.log(gameCount);
+			console.table(games[1]);
+		}
+	} else {
+		let deck = new Deck();
+		deck.cut(Math.floor(Math.random() * 364) + 51);
+		deck.burn();
+		while (deck.hasCards) {
+			games.push(play(deck));
+		}
+	}
+}
+
+playBtn.addEventListener('click', async () => {
+	if (playing) return;
+	playBtn.classList.add('disabled');
+	skipBtn.classList.add('disabled');
+	hideSummary();
+	playing = true;
+	await flipCard(games[gameCount]);
+	playing = false;
+	playBtn.classList.remove('disabled');
+	skipBtn.classList.remove('disabled');
+	drawDot(games[gameCount]);
+	increaseStats(games[gameCount].winner);
+	setSummary(games[gameCount]);
+	displaySummary();
+	calcWinnings(games[gameCount]);
+	clearBets();
+
+	gameCount++;
+	if (gameCount === games.length - 1) {
+		openModal('shuffle');
+		playing = true;
+	}
+	saveGame();
 });
 
-skipBtn.addEventListener("click", () => {
-  if (playing) return;
-  returnBets();
-  hideSummary();
-  clearBets();
-  flipCardsNoAni(games[gameCount]);
-  drawDot(games[gameCount]);
-  increaseStats(games[gameCount].winner);
-  setSummary(games[gameCount]);
-  displaySummary();
-  gameCount++;
-  if (gameCount >= games.length - 1) {
-    openModal("shuffle");
-  }
+function saveGame() {
+	localStorage.setItem('gameCount', gameCount);
+	localStorage.setItem('games', JSON.stringify(games));
+}
+
+skipBtn.addEventListener('click', () => {
+	if (playing) return;
+	returnBets();
+	hideSummary();
+	clearBets();
+	flipCardsNoAni(games[gameCount]);
+	drawDot(games[gameCount]);
+	increaseStats(games[gameCount].winner);
+	setSummary(games[gameCount]);
+	displaySummary();
+	gameCount++;
+
+	if (gameCount >= games.length - 1) {
+		openModal('shuffle');
+		playing = true;
+	}
+	saveGame();
 });
 
-newBtn.addEventListener("click", () => {
-  resetStats();
-  clear();
-  init();
-  hideSummary();
-  clearBoard();
-  closeModal();
+newBtn.addEventListener('click', () => {
+	localStorage.removeItem('games');
+	localStorage.removeItem('gameCount');
+	gameCount = 0;
+	games = [];
+	resetStats();
+	clear();
+	init();
+	hideSummary();
+	clearBoard();
+	closeModal();
 });
 
-helpBtn.addEventListener("click", () => {
-  openModal("help");
+helpBtn.addEventListener('click', () => {
+	openModal('help');
 });
--- scripts/dotMaker.js
+++ scripts/dotMaker.js
@@ -1,45 +1,51 @@
-const board = document.querySelector("#board");
+const board = document.querySelector('#board');
 
 let currentRun;
 let currentWinner = 0;
 
 function newRun() {
-  currentRun = document.createElement("div");
-  currentRun.classList.add("run");
-  board.appendChild(currentRun);
+	currentRun = document.createElement('div');
+	currentRun.classList.add('run');
+	board.appendChild(currentRun);
 }
 newRun();
 
 export default function drawDot(game) {
-  const dot = createDot(game);
-  currentRun.appendChild(dot);
-  setTimeout(() => {
-    dot.style.transform = "scale(1)";
-  }, 50);
+	const dot = createDot(game);
+	currentRun.appendChild(dot);
+	setTimeout(() => {
+		dot.style.transform = 'scale(1)';
+	}, 50);
+}
+
+export function drawDotNoAni(game) {
+	const dot = createDot(game);
+	dot.style.transform = 'scale(1)';
+	currentRun.appendChild(dot);
 }
 
 function createDot(game) {
-  if (game.winner !== "tie") {
-    if (currentWinner !== game.winner) {
-      newRun();
-    }
-    currentWinner = game.winner;
-  }
-  const dot = document.createElement("div");
-  dot.classList.add("dot");
-  dot.classList.add(game.winner + "-dot");
-  const inner = document.createElement("div");
-  //if (game.isForty) dot.classList.add("forty");
-  if (game.isNatural) {
-    inner.classList.add("natural");
-  } else if (game.bonus !== 0) {
-    inner.classList.add("bonus");
-    inner.innerText = game.bonus;
-  }
-  dot.appendChild(inner);
-  return dot;
+	if (game.winner !== 'tie') {
+		if (currentWinner !== game.winner) {
+			newRun();
+		}
+		currentWinner = game.winner;
+	}
+	const dot = document.createElement('div');
+	dot.classList.add('dot');
+	dot.classList.add(game.winner + '-dot');
+	const inner = document.createElement('div');
+
+	if (game.isNatural) {
+		inner.classList.add('natural');
+	} else if (game.bonus !== 0) {
+		inner.classList.add('bonus');
+		inner.innerText = game.bonus;
+	}
+	dot.appendChild(inner);
+	return dot;
 }
 
 export function clearBoard() {
-  board.innerHTML = "";
+	board.innerHTML = '';
 }
